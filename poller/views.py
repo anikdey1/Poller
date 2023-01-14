808ba0be757bd6913240bbd41a285fc5279335a1
Anik Dey <AnikDey0110@gmail.com>
2023-01-14 14:35:23 +0530

    Updated urls.py and views.py.

--- poller/views.py
+++ poller/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+
+from .models import Question
+
 
 # Create your views here.
 def index(request):
-	return HttpResponse("Hello, world. You are at the Poller index!")
+	latest_questions_list = Question.objects.order_by('-pub_date')[:5]
+	context = {'latest_questions_list': latest_questions_list}
+	return render(request, 'poller/index.html', context)
+
+def detail(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'poller/detail.html', {'question': question})
+
+def results(request, question_id):
+	response = "You are looking at the results of question %s."
+	return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+	return HttpResponse("You are voting on question %s." % question_id)
